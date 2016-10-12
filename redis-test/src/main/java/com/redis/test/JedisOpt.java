0244121f9489ed0017dd259159b1672754268d55
聂俊雷 <junlei.nie@hotmail.com>
2016-10-12 14:56:06 +0800

    redis 连接池 切片 哨兵模式配置使用

---
+++ redis-test/src/main/java/com/redis/test/JedisOpt.java
@@ -0,0 +1,97 @@
+package com.redis.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisSentinelPool;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+/**
+ * Hello world!
+ *
+ */
+@Component("jedisOpt")
+public class JedisOpt
+{
+
+    @Autowired
+    private ShardedJedisPool shardedJedisPool;
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    @Autowired
+    private JedisSentinelPool jedisSentinelPool;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ShardedJedis getShardedRedisClient() {
+        ShardedJedis shardJedis = null;
+        try {
+            shardJedis = shardedJedisPool.getResource();
+            return shardJedis;
+        } catch (Exception e) {
+            if (null != shardJedis)
+                shardJedis.close();
+        }
+        return null;
+    }
+
+    public void returnShardedResource(ShardedJedis shardedJedis) {
+        shardedJedis.close();
+    }
+
+    public void returnShardedResource(ShardedJedis shardedJedis, boolean broken) {
+        shardedJedis.close();
+    }
+
+    public Jedis getRedisClient() {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            return jedis;
+        } catch (Exception e) {
+            if (null != jedis)
+                jedis.close();
+        }
+        return null;
+    }
+
+    public void returnResource(Jedis jedis) {
+        jedis.close();
+    }
+
+    public void returnResource(Jedis jedis, boolean broken) {
+        jedis.close();
+    }
+
+    public Jedis getRedisSentinelClient() {
+        Jedis jedis = null;
+        try {
+            jedis = jedisSentinelPool.getResource();
+            return jedis;
+        } catch (Exception e) {
+            if (null != jedis)
+                jedis.close();
+        }
+        return null;
+    }
+
+    public void returnSentinelResource(Jedis jedis) {
+        jedis.close();
+    }
+
+    public void returnSentinelResource(Jedis jedis, boolean broken) {
+        jedis.close();
+    }
+
+    public StringRedisTemplate getSentinelClientOne(){
+        return stringRedisTemplate;
+    }
+}
---
+++ redis-test/src/test/java/com/redis/test/JedisTest.java
@@ -0,0 +1,84 @@
+package com.redis.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+import redis.clients.jedis.ShardedJedis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Unit test for simple JedisOpt.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/applicationContext.xml",
+        "classpath:spring/jedis_sharded_context.xml",
+        "classpath:spring/jedis_sentinel_context.xml",
+        "classpath:spring/jedis_sentinel_spring.xml",
+        "classpath:spring/jedis_pool_context.xml"})
+public class JedisTest
+{
+
+    @Autowired
+    JedisOpt jedisOpt;
+
+    @Test
+    public void testShardedJedis(){
+        ShardedJedis shardedJedis = jedisOpt.getShardedRedisClient();
+//        shardedJedis.set("name", "lilei");
+        System.out.println(shardedJedis.get("name"));
+        shardedJedis.append("name", "&hanmeimei");
+        System.out.println(shardedJedis.get("name"));
+    }
+
+    @Test
+    public void testJedis(){
+        Jedis jedis = jedisOpt.getRedisClient();
+        jedis.set("name", "lilei");
+        System.out.println(jedis.get("name"));
+        jedis.append("name", "&hanmeimei");
+        System.out.println(jedis.get("name"));
+    }
+
+    @Test
+    public void testSentinelJedis(){
+        Jedis jedis = jedisOpt.getRedisClient();
+        jedis.set("name", "lilei");
+        System.out.println(jedis.get("name"));
+        jedis.append("name", "&hanmeimei");
+        System.out.println(jedis.get("name"));
+    }
+
+    @Test
+    public void test() {
+        Set<String> sentinels = new HashSet<String>();
+        sentinels.add("127.0.0.1:6377");
+
+        /**
+         * masterName 分片的名称
+         * sentinels Redis Sentinel 服务地址列表
+         */
+        JedisSentinelPool poolA = new JedisSentinelPool("mymaster", sentinels);
+        //获取Jedis主服务器客户端实例
+        Jedis jedisA = poolA.getResource();
+        jedisA.set("key", "abc");
+
+
+        System.out.println("jedisA key:"+jedisA.get("key"));
+        //输出结果
+        //jedisA key:abc
+    }
+
+    @Test
+    public void testSentinel(){
+        StringRedisTemplate st = jedisOpt.getSentinelClientOne();
+        st.opsForValue().set("word", "hello");
+        System.out.println(st.opsForValue().get("word"));
+    }
+}
